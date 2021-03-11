a87cdd34f928c957e80141c266265d173a10c898
koleg68 <9326538@gmail.com>
2021-03-11 17:07:34 +0300

    update

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
+var images = document.querySelectorAll(".picture img.slide");
+var i = 0;
 
-	var images = document.querySelectorAll('.picture img.slide');
-	var i = 0;
+document.querySelector("img#prev").onclick = function () {
+  images[i].className = "slide";
 
-	document.querySelector('img#prev').onclick = function () {
+  i--;
 
-		images[i].className = 'slide';
+  if (i < 0) {
+    i = images.length - 1;
+  }
+  images[i].className = "active";
+};
 
-		i--;
+document.querySelector("img#next").onclick = function () {
+  images[i].className = "slide";
+  i++;
 
-		if (i < 0) {
-			i = images.length - 1;
-		}
-		images[i].className = 'active';
-	};
-
-	document.querySelector('img#next').onclick = function () {
-
-		images[i].className = 'slide';
-		i++;
-
-		if (i > images.length-1) {
-			i = 0;
-		}
-		images[i].className = 'active';
-
-	};
-
-
-jQuery(document).ready(function () {
-	jQuery("a.scrollto").click(function () {
-		elementClick = jQuery(this).attr("href");
-		destination = jQuery(elementClick).offset().top;
-		jQuery("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination }, 1100);
-		return false;
-	});
-});
+  if (i > images.length - 1) {
+    i = 0;
+  }
+  images[i].className = "active";
+};
 
 //Плавная прокрутка страницы вверх
 window.onload = function () {
-	var scrolled;
-	var timer;
+  var scrolled;
+  var timer;
 
-	document.getElementById('btn_top').onclick = function () {
-		scrolled = window.pageYOffset;
+  document.getElementById("btn_top").onclick = function () {
+    scrolled = window.pageYOffset;
+    scrollToTop();
+  };
 
-		scrollToTop();
-	};
+  function scrollToTop() {
+    if (scrolled > 0) {
+      window.scrollTo(0, scrolled);
+      scrolled = scrolled - 500; //100 скорость прокрутки
+      timer = setTimeout(scrollToTop, 100);
+    } else {
+      clearTimeout(timer);
+      window.scrollTo(0, 0);
+    }
+  }
 
-	function scrollToTop() {
-
-		if (scrolled > 0) {
-			window.scrollTo(0, scrolled);
-			scrolled = scrolled - 500; //100 скорость прокрутки
-			timer = setTimeout(scrollToTop, 100);
-		} else {
-			clearTimeout(timer);
-			window.scrollTo(0, 0);
-		}
-	}
-
-
-	window.onscroll = function () {
-		var top = document.getElementById('btn_top');
-		if (window.pageYOffset > 100) {
-			top.style.display = 'block';
-		} else {
-			top.style.display = 'none';
-		}
-	};
+  window.onscroll = function () {
+    var top = document.getElementById("btn_top");
+    if (window.pageYOffset > 400) {
+      top.style.display = "block";
+    } else {
+      top.style.display = "none";
+    }
+  };
 };
+
+document.querySelector(".menu-btn-mobile").onclick = function (e) {
+  e.preventDefault;
+  this.classList.toggle("menu-btn-mobile_active");
+  toggleMenu();
+};
+
+function toggleMenu() {
+  const menu = document.querySelectorAll("li.menu");
+  for (let i of menu) {
+    if (i.classList.contains("transparent")) {
+      i.classList.remove("transparent");
+      i.style.opacity = 0;
+    } else {
+      i.classList.add("transparent");
+      i.style.opacity = 1;
+    }
+  }
+}
